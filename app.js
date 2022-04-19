872ad53a97d13ff9bc7d022b905e8fb266bd4478
hilal <hilal8523@gmail.com>
2022-04-19 15:07:29 +0530

    function

---
+++ app.js
@@ -0,0 +1,72 @@
+// // // // console.log("hello world");
+
+// // // //  m
+
+// // // //  let a = 10 + 20; 
+// // // //  let b = 20 + 30;
+// // // //  let x = a + b;
+
+// // // //  console.log(x);
+
+// // // anonymous function
+
+// // // // function addd(){
+// // // //     var a=1000;
+// // // //     var b=2000;
+// // // //     var c=a-b
+// // // //     console.log(c);
+// // // //
+// // // // addd()
+// // // // function add(a, b) {
+// // // //     var sun = a % b;
+// // // //     console.log('this is sun ans ' , sun);
+// // // // }
+
+
+// // //invoking //callng  a function
+
+// // // // add(10,8)
+// // // // add(1,80)
+// // // // add(100,1)
+
+// // // // function was(a, b) {
+// // // //     let sun = a - b;
+// // // //     return sun;
+// // // // }
+
+// // // // var res = was('100', '200');
+// // // // console.log('the ans is ', res, ' u got it');
+// // //  let add = function (a , b) {
+// // //      var sun = a +b;
+// // //      return sun;
+// // //  }
+// // //  let res =add(10,20);
+// // //  console.log('this is function expression', res);
+
+
+// // //function expression 
+// //  var greet= function(a ,b){
+// //      var greet= a+b;
+// //      return greet;
+
+// //  }
+// //  var add = greet(1000,2000);
+// //  console.log('this ans  is ',add);
+
+
+// // let functionName = function(a,b,c){
+// //     let functionName =  ((a+b)-c);
+// //     return functionName;
+// // }
+// //  let cal =functionName(100,10,5);
+// //  console.log(cal);
+
+// //  //arrow function 
+// const add = () => {
+//     console.log("this is hilal learning arrow function");
+// }
+// add();
+
+// //single line function
+// const single=()=> "good nite hello world";
+// console.log(single() ," sleep well");
